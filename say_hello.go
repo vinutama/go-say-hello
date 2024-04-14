c687be0bf2316c41c68f2e647f50896ea1137f6c
vinutama <muhammadkhevin@gmail.com>
2024-04-14 16:22:43 +0700

    update say hello parameter age

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,9 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello " + name
+func SayHello(name string, age string) string {
+	return "Hello " + name + "My age is " + age
 }
+
+// git add, comm push
+// git tag v1.0.0
+// gpush v1.0.0
